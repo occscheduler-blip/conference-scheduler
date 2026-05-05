591de72128bf079c53652486fbf98f72e7c93681
ellanaglassner <eglassne@hamilton.edu>
2026-05-05 19:44:22 -0400

    updated quick actions in debugger

--- backend/app/scheduler/cp_sat.py
+++ backend/app/scheduler/cp_sat.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import logging
 import time
 from collections import defaultdict
-from datetime import timedelta
+from datetime import datetime, timedelta
 from math import ceil
 
 from ortools.sat.python import cp_model
